80cbd19189eb8572d5b0f76d59bb736216fd0860
German David <germanmarinrolong@gmail.com>
2023-01-26 15:59:50 -0500

    update

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,24 +7,46 @@ local fonts = {
 	iosevka_fixed = "Iosevka Fixed",
 	roboto_mono = "RobotoMono Nerd Font Mono",
 }
-
 local font = fonts.caskaydia_cove
 
+local weights = {
+	thin = "Thin",
+	extra_light = "ExtraLight",
+	light = "Light",
+	demi_light = "DemiLight",
+	book = "Book",
+	regular = "Regular",
+	medium = "Medium",
+	demi_bold = "DemiBold",
+	bold = "Bold",
+	extra_bold = "ExtraBold",
+	black = "Black",
+	extra_black = "ExtraBlack",
+}
+local font_weight = weights.demi_light
+
 return {
-	font = wezterm.font(font, { weight = "Regular", italic = false, bold = false }),
+	-- font
+	font = wezterm.font(font, { weight = font_weight, italic = false }),
 	font_rules = {
 		{
 			intensity = "Bold",
-			font = wezterm.font(font, { weight = "Regular" }),
+			font = wezterm.font(font, { weight = font_weight }),
 		},
 	},
 	font_size = 12.0,
-	enable_tab_bar = false,
+
+	-- tab bar
+	use_fancy_tab_bar = false,
+	enable_tab_bar = true,
+	hide_tab_bar_if_only_one_tab = true,
+	tab_bar_at_bottom = true,
+
 	window_decorations = "NONE",
 	default_cursor_style = "BlinkingBar",
 	force_reverse_video_cursor = true,
 	color_schemes = {
-		["Catppuccin Mocha Black"] = require("lua.colorschemes.catppuccin-mocha-black")
+		["Catppuccin Mocha Black"] = require("lua.colorschemes.catppuccin-mocha-black"),
 	},
 	color_scheme = "Catppuccin Mocha Black",
 }
